55a12b5ce4ea30ec321c984a30d1c358586784e5
Unknown <little_miss_kris07@hotmail.com>
2016-09-17 13:55:40 -0600

    Loops Assignment

---
+++ DGM 2670 Assignments/Assets/Loops.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour
+{
+        public string[] arielSisters = { "Aquata", "Andrina", "Arista", "Attina", "Adella", "Alana" };
+        public string[] hobbies = { "sing", "read", "swim"};
+
+    void Start()
+    {
+        foreach (string item in arielSisters)
+        {
+            foreach (string hobby in hobbies)
+            {
+                print("Hello! My name is " + item + ". I love to " + hobby + "!");
+            }
+        }
+    }
+}
