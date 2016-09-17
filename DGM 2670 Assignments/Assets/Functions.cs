ad360e61f417ff7c1da5db9ed4b64e218697f6d5
Unknown <little_miss_kris07@hotmail.com>
2016-09-16 18:35:42 -0600

    Functions Assignment

---
+++ DGM 2670 Assignments/Assets/Functions.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Functions : MonoBehaviour
+{
+
+    float hoursWorked = 0;
+    float dollarsPerHour = 1.00f;
+    float moneyEarned;
+    float hoursToWorkForToyCar;
+    public float toyCarCost = 10.00f;
+    float FirstHourPaycheck;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        MoneySavedForToyCar();
+    }
+
+
+    void MoneySavedForToyCar()
+    {
+        hoursToWorkForToyCar = toyCarCost / dollarsPerHour;
+        FirstHourPaycheck = dollarsPerHour;
+
+        while (hoursWorked < hoursToWorkForToyCar)
+        {
+            moneyEarned = FirstHourPaycheck + hoursWorked * dollarsPerHour;
+            print("I don't have enough money for a toy car.");
+            hoursWorked++;
+        }
+
+        if (moneyEarned == toyCarCost)
+        {
+            print("I can buy a toy car!");
+        }
+
+    }
+}
