a2bc0062d8ab374f80361d0fb41e708a28f1b249
Unknown <little_miss_kris07@hotmail.com>
2016-09-17 11:09:15 -0600

    Conventions and Syntax Assignment

---
+++ DGM 2670 Assignments/Assets/ConventionsAndSyntax.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConventionsAndSyntax : MonoBehaviour
+{
+
+    // Double forward slash allows you to do one line of comment.
+    /* Forward slash and asterisk allow you to do multiple lines of comments.
+     *Here is another comment.
+     *Asterisk and forward slash ends the comment.
+     */
+    public bool harryPotter = true;
+
+	void Start ()
+    {
+        WhereIsHarryPotter();
+	}
+
+    void WhereIsHarryPotter ()
+    {
+        if(harryPotter == true)
+        {
+            //End all statements that don't have curly brackets with a semicolon.
+            print("Harry Potter is at Hogwarts");
+        }
+        else
+        {
+            print("Harry Potter is at the Dursley's house.");
+        }
+    }
+}
