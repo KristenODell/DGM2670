1c0a1275fc269be61275c44155cd398ca63cf5ed
Unknown <little_miss_kris07@hotmail.com>
2016-09-23 20:54:25 -0600

    Arrays and Lists Assignment

---
+++ DGM 2670 Assignments/Assets/Assignments 6 - 8/ArraysAndLists.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArraysAndLists : MonoBehaviour
+{
+    //Pokemon Array List
+    public GameObject[] pokemon;
+
+    void Start()
+    {
+        pokemon = GameObject.FindGameObjectsWithTag("Pokemon");
+    }
+
+    void OnTriggerEnter()
+    {
+        for (int i = 0; i < pokemon.Length; i++)
+        {
+            print("You found " + pokemon.Length + " pokeballs. One is a " + pokemon[i].name);
+        }
+
+    }
+}
---
+++ DGM 2670 Assignments/Assets/Assignments 6 - 8/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController myCC;
+    private Vector3 tempPos;
+    public float speed = 1;
+    public float gravity = 9.81f;
+    public float jumpSpeed = 1;
+    public int jumpCount = 0;
+    public int jumpCountMax = 2;
+
+
+
+    void Start()
+    {
+        myCC = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < jumpCountMax - 1)
+        {
+            jumpCount++;
+            tempPos.y = jumpSpeed;
+        }
+
+        if (myCC.isGrounded)
+        {
+            jumpCount = 0;
+        }
+
+        tempPos.y -= gravity;
+        tempPos.x = speed * Input.GetAxis("Horizontal");
+        myCC.Move(tempPos * Time.deltaTime);
+
+    }
+}
