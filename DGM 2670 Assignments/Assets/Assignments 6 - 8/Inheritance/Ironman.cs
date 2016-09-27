545eef1b7051f26d93e01ef31438c74f6bc0c9d4
Unknown <little_miss_kris07@hotmail.com>
2016-09-26 18:34:52 -0600

    Inheritance Assignment

---
+++ DGM 2670 Assignments/Assets/Assignments 6 - 8/Inheritance/Ironman.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ironman : Superhero
+{
+    public Ironman ()
+    {
+        awesomeness = 100;
+        Debug.Log("Ironman gets " + awesomeness + " points.");
+    }
+    public Ironman(int newAwesomeness) : base(newAwesomeness)
+    {
+        Debug.Log("Ironman is awesome.");
+    }
+}
---
+++ DGM 2670 Assignments/Assets/Assignments 6 - 8/Inheritance/Superhero.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Superhero
+{
+    public int awesomeness;
+
+    public Superhero ()
+    {
+        awesomeness = 10;
+        Debug.Log("Superhero gets " + awesomeness + " points.");
+    }
+    public Superhero (int newAwesomeness)
+    {
+        awesomeness = newAwesomeness;
+        Debug.Log("Superhero is awesome.");
+    }
+}
+
+
